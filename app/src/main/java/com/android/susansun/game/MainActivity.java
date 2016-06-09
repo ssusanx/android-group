3c737daa4595187cceef6065aa9bb7e5e1407c95
louiseperez <lperez7@calstatela.edu>
2016-06-09 16:57:55 -0700

    game update

--- app/src/main/java/com/android/susansun/game/MainActivity.java
+++ app/src/main/java/com/android/susansun/game/MainActivity.java
@@ -1,53 +1,82 @@
 package com.android.susansun.game;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button drag;
+    LinearLayout drop;
+    TextView text, sucess;
+    int total, failure = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.player1_place_ships);
+        drag = (Button)findViewById(R.id.one);
+        drop = (LinearLayout)findViewById(R.id.bottomlinear1);
+        text = (TextView)findViewById(R.id.Total);
+        sucess = (TextView)findViewById(R.id.Sucess);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        drop.setOnDragListener(new View.OnDragListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onDrag(View v, DragEvent event) {
+                // TODO Auto-generated method stub
+                final int action = event.getAction();
+                switch(action) {
+
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        break;
+
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        break;
+
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        break;
+
+                    case DragEvent.ACTION_DROP:{
+                        failure = failure+1;
+                        return(true);
+                    }
+
+                    case DragEvent.ACTION_DRAG_ENDED:{
+                        total = total +1;
+                        int suc = total - failure;
+                        sucess.setText("Sucessful Drops :" + suc);
+                        text.setText("Total Drops: " + total);
+                        return(true);
+
+                    }
+
+                    default:
+                        break;
+                }
+                return true;
+            }});
+        drag.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent arg1) {
+                // TODO Auto-generated method stub
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadow = new View.DragShadowBuilder(drag);
+                v.startDrag(data, shadow, null, 0);
+                return false;
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/android/susansun/game/Splash.java
@@ -0,0 +1,38 @@
+package com.android.susansun.game;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Totten on 6/9/2016.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
